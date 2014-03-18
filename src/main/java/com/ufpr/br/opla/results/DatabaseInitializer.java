c3461d6dd9caf96b6eedb5ec12761dbd92b027d3
Edipo Luis Federle <edipofederle@gmail.com>
2014-03-18 14:36:51 -0300

    Cleanup. add opla.db initial database

--- src/main/java/com/ufpr/br/opla/results/DatabaseInitializer.java
+++ src/main/java/com/ufpr/br/opla/results/DatabaseInitializer.java
@@ -1,5 +1,6 @@
 package com.ufpr.br.opla.results;
 
+import com.ufpr.br.opla.exceptions.MissingConfigurationException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,26 +15,38 @@ import java.util.logging.Logger;
 public class DatabaseInitializer {
     
     private static Connection connection;
-    private static final String PATH_TO_DATABASE = "resources/opla.db";
+    private static String pathDatabase = "";
 
     /**
      * Create a connection with database and returns a Statement to working with.
      * 
      * @return
      * @throws ClassNotFoundException 
+     * @throws java.sql.SQLException 
+     * @throws com.ufpr.br.opla.exceptions.MissingConfigurationException 
      */
-    public static Statement initialize() throws ClassNotFoundException {
+    public static Statement initialize() throws MissingConfigurationException, SQLException, ClassNotFoundException {
+        
+        if ("".equals(pathDatabase))
+            throw new MissingConfigurationException("Path to database should not be blank");
+       
+        
+        return getConnection();
+    }  
+
+    private static Statement getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
         Statement statement = null;
-               
-        try{
-            connection = DriverManager.getConnection("jdbc:sqlite:"+PATH_TO_DATABASE);
-            statement = connection.createStatement();
-            statement.setQueryTimeout(30); //in seconds
-        }catch(SQLException e){
-            Logger.getLogger(DatabaseInitializer.class.getName()).log(Level.SEVERE, null, "Ops, Error when try initialize DB: " +e);
-        }
+        connection = DriverManager.getConnection("jdbc:sqlite:"+pathDatabase);
+        statement = connection.createStatement();
+        statement.setQueryTimeout(30); //in seconds
+        
         
         return statement;
-    }  
+    }
+
+    public static void setPathToDb(String pathToDB) {
+        pathDatabase = pathToDB;
+    }
+
 }
--- src/test/java/InitializeDatabaseTest.java
+++ src/test/java/InitializeDatabaseTest.java
@@ -1,10 +1,9 @@
-
-
-
+import com.ufpr.br.opla.exceptions.MissingConfigurationException;
 import com.ufpr.br.opla.results.DatabaseInitializer;
 import java.io.File;
 import org.junit.After;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 
 /*
@@ -19,26 +18,39 @@ import org.junit.Test;
  */
 public class InitializeDatabaseTest {
     
-    @After
-    public void tearDown(){
-        File db = new File("resources/opla.db");
-        db.delete();
+    private static final String PATH_TO_DATABASE = "src/test/resources/opla.db";
+    
+    @Before
+    public void setUp(){
+        DatabaseInitializer.setPathToDb(PATH_TO_DATABASE);
+    }
+    
+//    @After
+//    public void cleanUp(){
+//        File db = new File(PATH_TO_DATABASE);
+//        db.delete();
+//    }
+    
+    @Test(expected=MissingConfigurationException.class)
+    public void shouldExceptionWhenDontHavePathToDatabaseFile() throws  Exception{
+        DatabaseInitializer.setPathToDb("");
+        DatabaseInitializer.initialize();
+        
     }
     
     @Test
     public void shouldCreateDBOnlyOnce() throws Exception{
         DatabaseInitializer.initialize();
         
-        File db = new File("resources/opla.db");
+        File db = new File(PATH_TO_DATABASE);
         
         assertTrue(db.exists());
         long timestamp = db.lastModified();
         
-        File db2 = new File("resources/opla.db");
+        File db2 = new File(PATH_TO_DATABASE);
         
         DatabaseInitializer.initialize();
         assertTrue(timestamp == db2.lastModified() );
-        
     }
     
 }
