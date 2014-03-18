d1cc30e5440879ae4275105dbf7d36806aa37e3d
Edipo Luis Federle <edipofederle@gmail.com>
2014-03-18 12:20:26 -0300

    add sqlite lib

---
+++ src/main/java/com/ufpr/br/opla/results/FunResults.java
@@ -0,0 +1,18 @@
+/*
+Fun files is where objective values are stored.
+*/
+package com.ufpr.br.opla.results;
+
+/**
+ *
+ * @author elf
+ */
+public class FunResults {
+    
+    /**
+     * Objectives are stored on a single attribute,
+     * the values are separated by pipe ("|").
+     *  
+     */
+    private String objetives;
+}
